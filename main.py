6934a3cada5619f9e3b51d336146a6d03e120021
Sonnik9 <nikolas.smsttt@gmail.com>
2023-05-15 19:19:16 -0400

    add eval to uagent

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ def uaRefresh():
 # //////////////spart headers start///////////////////////////////
 
 def random_headers(uagent): 
+    try:
+        uagent = eval(uagent)
+    except:
+        uagent = uagent
     
     desktop_accept = [
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
