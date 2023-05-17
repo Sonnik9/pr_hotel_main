c5428382040f0c59a5f3e892f2e45dde55b140fe
Sonnik9 <nikolas.smsttt@gmail.com>
2023-05-17 14:58:12 -0400

    changing interval

--- main.py
+++ main.py
@@ -480,8 +480,8 @@ def main():
         'n1': 0,
         'n2': 0,
         'interval': 1000,
-        'from_item': 1000,
-        'len_items': 10000,
+        'from_item': 10000,
+        'len_items': 20000,
         'counter': 0,
         'flag_end_cycles': False,
         'cpu_count': 40
