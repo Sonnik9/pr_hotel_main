4425dae55e90e9e5024397718dfe96f18f0c90ec
Sonnik9 <nikolas.smsttt@gmail.com>
2023-05-29 10:01:13 -0400

    reader fixing fotos

--- db_all/db_reader.py
+++ db_all/db_reader.py
@@ -14,6 +14,89 @@ def db_opener(n1, n2):
             'database': config_real.database,      
         }
 
+        for _ in range(3):
+
+            try:
+                conn = mysql.connector.connect(**config)      
+                print("REader connection established fotos")
+                break
+            except Error as e:
+                print(f"Error connecting to MySQL: {e}")
+                time.sleep(3)
+                continue           
+        
+        try:
+            cursor = conn.cursor() 
+        except Error as e:
+            print(f"Error connecting to MySQL: {e}")
+            # time.sleep(3)
+            # continue
+
+        try:
+            select_query  = ("SELECT id, hotel_id, url, fotos FROM upz_hotels "
+            f"WHERE id BETWEEN {n1} AND {n2} "
+            )
+            cursor.execute(select_query)
+            hotels_data = cursor.fetchall()
+        except Exception as e:
+            print(f"db_reader___str46: {e}")
+            # time.sleep(3)
+            # continue
+
+        try:
+            for id, hotel_id, url, fotos in hotels_data:
+                data_DB.append({
+                    'id': id,
+                    'hotel_id': hotel_id,
+                    'url': url,
+                    'fotos': fotos
+                })
+                # print(hotel_id)
+        except Exception as ex:
+            print(f"source_DB_hotel_46str___{ex}")
+            # time.sleep(3)
+            # continue    
+
+        try:
+            cursor.close()
+            conn.close()
+            # print(result[100000:100002])
+        except Error as e:
+            print(f"Error connecting to MySQL: {e}")
+            # time.sleep(3)
+            # continue
+        try:
+            print(f"data_DB _____{data_DB[0]}")
+        except:
+            pass
+        return data_DB
+            # print(f"data_DB _____{data_DB[-1]}")
+            # print(f"data_DB _____{data_DB[-1]}")
+    except:
+        return data_DB
+        
+
+# python db_reader.py
+# python -m db_all.db_reader
+
+
+
+def db_opener(n1, n2):
+    data_DB = []
+    try:
+        import time
+        import mysql.connector
+        from mysql.connector import connect, Error 
+        from . import config_real
+        print(n1, n2)       
+        config = {
+            'user': config_real.user,
+            'password': config_real.password,
+            'host': config_real.host,
+            'port': config_real.port,
+            'database': config_real.database,      
+        }
+
         for _ in range(5):
 
             try:
