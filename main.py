49372c9b273933a3230d095a5e1d4807546d4793
Sonnik9 <nikolas.smsttt@gmail.com>
2023-05-29 10:01:57 -0400

    70000 from fotos

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def main():
         'n1': 0,
         'n2': 0,
         'interval': 1000,
-        'from_item': 0,
+        'from_item': 70000,
         'len_items': 100000,
         'counter': 0,
         'flag_end_cycles': False,
